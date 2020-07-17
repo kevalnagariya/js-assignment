d3913ebde0f23a6952fa5beacdd82f3edf342d73
kevalnagariya <32120110+kevalnagariya@users.noreply.github.com>
2020-07-17 11:43:14 +0530

    Js assignment 3
    Not able to understand question 2

---
+++ Day3/if.js
@@ -0,0 +1,6 @@
+
+var marks;
+if (marks=50){
+    console.log("Marks are " +marks+ " grade is B");
+}
+    
---
+++ Day3/sr.js
@@ -0,0 +1,18 @@
+ 
+function marksgrade()
+{
+    
+    switch (grade=50)
+    {
+        case grade >= 51 :
+            console.log("Marks are " +grade+ " grade is A");
+        break;
+        case grade=50:
+            console.log("Marks are " +grade+ " grade is B");
+        break;
+        default:
+            console.log("You are failed");
+    }
+}
+
+
